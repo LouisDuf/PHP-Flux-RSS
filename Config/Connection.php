1f8f6a853542594b4fa0b4fb6c9b8562286cedf3
louwar <loup6378@gmail.com>
2022-11-25 11:28:40 +0100

    Add config project and Add vu listNews

--- Config/Connection.php
+++ Config/Connection.php
@@ -3,10 +3,10 @@
 
 		private $stmt;
 
-		public function __construct(string $dsn, string $username, string $password) { 
-
-		parent::__construct($dsn,$username,$password); 
-		$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		public function __construct(string $dsn, string $username, string $password)
+		{
+			parent::__construct($dsn,$username,$password);
+			$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} 
 		 
 
@@ -14,19 +14,17 @@
 		       * @param array $parameters * 
 		       * @return bool Returns `true` on success, `false` otherwise 
 		*/
-		public function executeQuery(string $query, array $parameters = []) : bool{ 
+		public function executeQuery(string $query, array $parameters = []) : bool
+		{
 			$this->stmt = parent::prepare($query); 
 			foreach ($parameters as $name => $value) { 
 			 $this->stmt->bindValue($name, $value[0], $value[1]); 
-			} 
-
+			}
 			return $this->stmt->execute(); 
 		}
 
 		public function getResults() : array {
 		 return $this->stmt->fetchall();
-
 		}
 	}
-
 ?>
