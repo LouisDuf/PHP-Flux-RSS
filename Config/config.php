65543bb6ecb080e8405e6b4100717d4ac2a8d125
louwar <loup6378@gmail.com>
2022-12-02 09:17:15 +0100

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -13,6 +13,10 @@ $base="localhost;dbname=louisdb";
 $login="louis";
 $mdp="dbloulou";
 
+//$base="mysql:host=londres.uca.local;dbname=dbreregnault";
+//$login="reregnault";
+//$mdp="achanger";
+
 //Vues
 $vues['erreur']='Vue/erreur.php';
 $vues['accueil']='Vue/listeNews.php';
