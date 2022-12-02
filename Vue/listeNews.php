86ff51d0bdb4ddbc28ae9ae1270f7f2d10013359
louwar <loup6378@gmail.com>
2022-12-02 09:05:25 +0100

    Add vu listeNews

--- Vue/listeNews.php
+++ Vue/listeNews.php
@@ -3,10 +3,90 @@
     <head>
         <title>Liste - News</title>
         <meta charset="utf-8" />
-        <link rel=icon href="imgs/Minecraft-logos.png"/>
+        <link rel="stylesheet" type="text/css" href="../style.css">
+        <link rel=icon href="../imgs/Minecraft-logos.png"/>
     </head>
-
     <body>
-        <p>Essayez de mettre du code html dans nom -> Correspond à une attaque de type injection</p>
+        <div class="navBar">
+            <h1>Notre site web</h1>
+        </div>
+        
+        <div class="page-content page-container" id="page-content">
+            <div class="padding">
+                <div class="row">
+                    <div class="col-sm-6">
+                    
+                        <div class="list list-row block">
+                            <div class="list-item" data-id="19">
+                                <?php
+                                    // on v�rifie les donn�es provenant du mod�le
+                                    if (isset($tabNews))
+                                    {
+                                        foreach ($tabNews as &$News) {
+                                            echo '<a href="'.$News->getUrl().'" data-abc="true">';
+                                        } 
+                                    } 
+                                    else
+                                    {
+                                        require("erreur");
+                                    }
+                                ?>                               
+                                    <div><a href="#" data-abc="true"><span class="w-48 avatar gd-warning">S</span></a></div>
+                                    <div class="flex">
+                                        <a href="#" class="item-author text-color" data-abc="true"> 
+                                            <?php
+                                                // on v�rifie les donn�es provenant du mod�le
+                                                if (isset($tabNews))
+                                                {
+                                                    foreach ($tabNews as &$News) {
+                                                        echo "<p>".$News->getTitle()."<p>";
+                                                    } 
+                                                } 
+                                                else
+                                                {
+                                                    require("erreur");
+                                                }
+                                            ?>
+                                        </a>
+                                        <div class="item-except text-muted text-sm h-1x">
+                                            <?php
+                                                // on v�rifie les donn�es provenant du mod�le
+                                                if (isset($tabNews))
+                                                {
+                                                    foreach ($tabNews as &$News) {
+                                                        echo "<p>".$News->getDescription()."<p>";
+                                                    } 
+                                                } 
+                                                else
+                                                {
+                                                    require("erreur");
+                                                }
+                                            ?>
+                                        </div>
+                                    </div>
+                                    <div class="no-wrap">
+                                        <div class="item-date text-muted text-sm d-none d-md-block">
+                                            <?php
+                                                // on v�rifie les donn�es provenant du mod�le
+                                                if (isset($tabNews))
+                                                {
+                                                    foreach ($tabNews as &$News) {
+                                                        echo "<p>".$News->getDate()."<p>";
+                                                    } 
+                                                } 
+                                                else
+                                                {
+                                                    require("erreur");
+                                                }
+                                            ?>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
