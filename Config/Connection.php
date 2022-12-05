4ec8db6710c9a2f5e6b1fdda9f2fc36f2c7d85e2
louwar <loup6378@gmail.com>
2022-12-05 23:22:46 +0100

    Add nameSpace AND class

--- Config/Connection.php
+++ Config/Connection.php
@@ -1,4 +1,8 @@
 <?php
+
+namespace config;
+
+
 	class Connection extends PDO { 
 
 		private $stmt;
---
+++ Modele/FluxModel.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LouisDuf
+ * Date: 05/12/2022
+ * Time: 23:17
+ */
+
+namespace modele;
+
+class FluxModele {
+
+}
